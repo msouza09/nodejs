c69bacf7b1380da52063477edfbe761225989980
Matheus <szmatheussouza@gmail.com>
2024-05-05 19:15:29 -0300

    Implementação do  fastify

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require('express');
+const fastify = require('fastify')({logger:true});
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
+fastify.get('/', () => {
+  return 'Hello World';
 });
 
-app.listen(PORT, HOST);
+fastify.get('/teste', () => {
+  return 'Hello Matheus';
+});
+
+fastify.get('/node', () => {
+  return 'Faz o L';
+});
+
+
+fastify.listen(PORT, HOST, (err,address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+});
+
